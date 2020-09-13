38fcb3139ba0314ef95693334fba259c5af15d0e
NJ1219 <niteshjain1219@gmail.com>
2020-09-13 16:40:46 -0400

    uploading files for roboadvisor

--- RoboAdvisor Project/Starter_Files/lambda_function.py
+++ RoboAdvisor Project/Starter_Files/lambda_function.py
@@ -1,6 +1,7 @@
 ### Required Libraries ###
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from botocore.vendored import requests
 
 ### Functionality Helper Functions ###
 def parse_int(n):
@@ -25,6 +26,40 @@ def build_validation_result(is_valid, violated_slot, message_content):
         "violatedSlot": violated_slot,
         "message": {"contentType": "PlainText", "content": message_content},
     }
+
+
+def validate_data(age, investment_amount, intent_request):
+    """
+    Validates the data provided by the user.
+    """
+
+    # Validate that the user is less than 65 years old
+    if age is not None:
+        if age > 65 or age < 0:
+            return build_validation_result(
+                False,
+                "age",
+                "You should be less than 65 years old to use this service, "
+                "please provide a different age.",
+            )
+
+    # Validate the investment amount,
+    # it should be equal or greater than 5000
+    if investment_amount is not None:
+        investment_amount = parse_int(
+            investment_amount
+        )  # Since parameters are strings it's important to cast values
+        if investment_amount < 5000:
+            return build_validation_result(
+                False,
+                "investmentAmount",
+                "The amount to invest should be greater than 5000, "
+                "please provide a correct amount in CAD to invest.",
+            )
+
+    # A True results is returned if age or amount are valid
+    return build_validation_result(True, None, None)
+
 
 
 ### Dialog Actions Helper Functions ###
@@ -80,6 +115,9 @@ def close(session_attributes, fulfillment_state, message):
     return response
 
 
+
+
+
 ### Intents Handlers ###
 def recommend_portfolio(intent_request):
     """
@@ -92,14 +130,40 @@ def recommend_portfolio(intent_request):
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
 
+    if age: 
+        age = int(age)
+        
+    if risk_level:
+        risk_level = risk_level.lower()
+
+    
     if source == "DialogCodeHook":
+
+        ### DATA VALIDATION CODE STARTS HERE ###
+         # Gets all the slots
+        slots = get_slots(intent_request)
+
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(age, investment_amount, intent_request)
+
         # Perform basic validation on the supplied input slots.
         # Use the elicitSlot dialog action to re-prompt
         # for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]] = None  # Cleans invalid slot
 
-        ### YOUR DATA VALIDATION CODE STARTS HERE ###
+             # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+        
+        
 
-        ### YOUR DATA VALIDATION CODE ENDS HERE ###
+        ### DATA VALIDATION CODE ENDS HERE ###
 
         # Fetch current session attibutes
         output_session_attributes = intent_request["sessionAttributes"]
@@ -108,9 +172,18 @@ def recommend_portfolio(intent_request):
 
     # Get the initial investment recommendation
 
-    ### YOUR FINAL INVESTMENT RECOMMENDATION CODE STARTS HERE ###
-
-    ### YOUR FINAL INVESTMENT RECOMMENDATION CODE ENDS HERE ###
+    if risk_level == 'none':
+        initial_recommendation = '100% bonds (AGG), 0% equities (SPY)'
+    elif risk_level == 'very low':
+        initial_recommendation = '80% bonds (AGG), 20% equities (SPY)'
+    elif risk_level == 'low':
+        initial_recommendation = '60% bonds (AGG), 40% equities (SPY)'
+    elif risk_level == 'medium':
+        initial_recommendation = '40% bonds (AGG), 60% equities (SPY)'
+    elif risk_level == 'high':
+        initial_recommendation = '20% bonds (AGG), 80% equities (SPY)'
+    elif risk_level == 'very high':
+        initial_recommendation = '0% bonds (AGG), 100% equities (SPY)'
 
     # Return a message with the initial recommendation based on the risk level.
     return close(
